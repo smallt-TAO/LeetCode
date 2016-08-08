14ac18ca1f06491bcc73c08af64be2338845c43d
smallt-TAO <llt5662720@163.com>
2016-08-08 19:14:05 +0800

    Answer
    Answer of Remove Element.

---
+++ Array/Remove Element/Solution.cpp
@@ -0,0 +1,14 @@
+//LeetCode
+//Time complexity O(n),Space complexity O(1)
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+};
