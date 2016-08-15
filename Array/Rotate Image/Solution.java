b1816e4d68ab4aca9847ce0e12b54a237f809fab
smallt-TAO <llt5662720@163.com>
2016-08-15 22:52:01 +0800

    Add
    Add answer for Rotate Image.

---
+++ Array/Rotate Image/Solution.java
@@ -0,0 +1,22 @@
+// LeetCode, Rotate Image
+// Time complexity O(n^2)
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int temp = 0;
+        
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n - i; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][n - 1 - i];
+                matrix[n - 1 - j][n - 1 - i] = temp;
+            }
+
+        for (int i = 0; i < n / 2; i++)
+            for (int j = 0; j < n; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - i][j];
+                matrix[n - 1 - i][j] = temp;
+            }
+    }
+}
