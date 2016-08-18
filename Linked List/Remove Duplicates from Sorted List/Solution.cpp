5e435955af5504320b45bfe55cbdd152ad9a7414
smallt-TAO <llt5662720@163.com>
2016-08-18 22:17:02 +0800

    Add
    Add answer for Remove Duplicates from Sorted List.

---
+++ Linked List/Remove Duplicates from Sorted List/Solution.cpp
@@ -0,0 +1,26 @@
+// LeetCode, Remove Duplicates from Sorted List
+// Time complexity O(n)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == NULL) return NULL;
+        
+        ListNode *node = head;
+        while (node->next != NULL) {
+            if (node->val == node->next->val) {
+                node->next = node->next->next;
+            } else {
+                node = node->next;
+            }
+        }
+        return head;
+    }
+};
---
+++ Linked List/Remove Duplicates from Sorted List/Solution.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            return None
+        curr = head
+        while curr != None and curr.next != None:
+            if (curr.val == curr.next.val):
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
+        
