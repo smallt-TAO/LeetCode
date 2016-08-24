f21c9a76d6ebca4428e3a6c7f5c053a345f4dd95
smallt-TAO <llt5662720@163.com>
2016-08-24 13:56:42 +0800

    Update

--- String/Wildcard Matching/Solution.cpp
+++ String/Wildcard Matching/Solution.cpp
@@ -9,18 +9,18 @@ public:
         if (m > 30000) return false;
         
         bool re[m + 1] = {false};
-        re[0] = true;                         //默认第零位为匹配，因为有可能p[0]==*
+        re[0] = true;                         // assmue true, may p[0]==*.
         for (int i = 0; i < n; i++) {
-            if (p[i] == '*'){                 //判断是否为*，*需要特殊处理
-                for (int k = 0; k < m; k++){
-                    re[k + 1] = re[k + 1] || re[k]; //如果已经为真，则继续为真；否则要取决于前一位。
+            if (p[i] == '*') {
+                for (int k = 0; k < m; k++) {
+                    re[k + 1] = re[k + 1] || re[k]; // if ture go true; otherwise depend on pre.
                 }
             } else {
-                for (int j = m; j > 0; j--){      //取决于前一位，并且要求这一位匹配
+                for (int j = m; j > 0; j--) {
                     re[j] = re[j - 1] && (p[i] == s[j - 1] || p[i] == '?');
                 }
             }
-            re[0] = re[0] && p[i]=='*';       //如果该位置不为*，则第零位不可为真
+            re[0] = re[0] && p[i]=='*';       // if not *, re[0] is false.
         }
         return re[m];
     }
