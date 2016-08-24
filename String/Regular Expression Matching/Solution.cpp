ab1b0a842b64c9a3827d70e296916f6e4f7900ed
smallt-TAO <llt5662720@163.com>
2016-08-24 10:55:13 +0800

    Optimization
    Optimization the answer for Regular Expression Match c++ and java.

--- String/Regular Expression Matching/Solution.cpp
+++ String/Regular Expression Matching/Solution.cpp
@@ -3,24 +3,22 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        if (s.length() == 0) {
-            if (p.length() & 1) return false;
-            else {
-                for (int i = 1; i < p.length(); i += 2) {
-                    if (p[i] != '*') return false;
-                }
-            }
-            return true;
-        }
-        if (p.length() == 0) return false;
-        if (p.length() > 1 && p[1] == '*') {
-            if (p[0] == '.' || s[0] == p[0]) {
-                return isMatch(s.substr(1), p) || isMatch(s, p.substr(2));
-            } else return isMatch(s, p.substr(2));
-        } else {
-            if (p[0] == '.' || s[0] == p[0]) {
-                return isMatch(s.substr(1), p.substr(1));
-            } else return false;
-        }
+        int m = s.size(), n = p.size();
+        bool f[m + 1][n + 1] = {false}; 
+        
+        f[0][0] = true;
+        // p[0.., j - 3, j - 2, j - 1] matches empty iff p[j - 1] is '*' and p[0..j - 3] matches empty
+        for (int j = 1; j <= n; j++)
+            f[0][j] = j > 1 && '*' == p[j - 1] && f[0][j - 2];
+        
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (p[j - 1] != '*')
+                    f[i][j] = f[i - 1][j - 1] && (s[i - 1] == p[j - 1] || '.' == p[j - 1]);
+                else
+                    // p[0] cannot be '*' so no need to check "j > 1" here
+                    f[i][j] = f[i][j - 2] || (s[i - 1] == p[j - 2] || '.' == p[j - 2]) && f[i - 1][j];
+        
+        return f[m][n];
     }
 };
--- String/Regular Expression Matching/Solution.java
+++ String/Regular Expression Matching/Solution.java
@@ -1,36 +1,46 @@
 // LeetCode, Regular Expression Matching
-// Time complexity O(n)
+// Time complexity O(n^2)
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int m = s.length(), n = p.length();
-        boolean[] res = new boolean[n + 1];
-        res[0] = true;
-        
-        int i, j;
-		for (j = 2; j <= n; j++)
-			res[j] = res[j - 2] && p.charAt(j - 1) == '*';
-
-		char pc, sc, tc;
-		boolean pre, cur; // pre=res[i - 1][j - 1], cur=res[i-1][j]
-
-		for (i = 1; i <= m; i++) {
-			pre = res[0];
-			res[0] = false;
-			sc = s.charAt(i - 1);
-
-			for (j = 1; j <= n; j++) {
-				cur = res[j];
-				pc = p.charAt(j - 1);
-				if (pc != '*')
-					res[j] = pre && (sc == pc || pc == '.');
-				else {
-					// pc == '*' then it has a preceding char, i.e. j>1
-					tc = p.charAt(j - 2);
-					res[j] = res[j - 2] || (res[j] && (sc == tc || tc == '.'));
-				}
-				pre = cur;
-			}
-		}
-		return res[n];
+        /*
+        1, If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
+        2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
+        3, If p.charAt(j) == '*': 
+               here are two sub conditions:
+               1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
+               2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
+                              dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a 
+                           or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
+                           or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+        */
+        if(s == null || p == null) {
+            return false;
+        }
+        boolean[][] state = new boolean[s.length() + 1][p.length() + 1];
+        state[0][0] = true;
+        // no need to initialize state[i][0] as false
+        // initialize state[0][j]
+        for (int j = 1; j < state[0].length; j++) {
+            if (p.charAt(j - 1) == '*') {
+                if (state[0][j - 1] || (j > 1 && state[0][j - 2])) {
+                    state[0][j] = true;
+                }
+            } 
+        }
+        for (int i = 1; i < state.length; i++) {
+            for (int j = 1; j < state[0].length; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    state[i][j] = state[i - 1][j - 1];
+                }
+                if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        state[i][j] = state[i][j - 2];
+                    } else {
+                        state[i][j] = state[i - 1][j] || state[i][j - 1] || state[i][j - 2];
+                    }
+                }
+            }
+        }
+        return state[s.length()][p.length()];
     }
 }
