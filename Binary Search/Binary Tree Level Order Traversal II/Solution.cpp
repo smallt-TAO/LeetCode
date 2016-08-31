1f38a98fcf9f27c781788130e981f80a67c4bc74
smallt-TAO <llt5662720@163.com>
2016-08-31 21:44:39 +0800

    Answer
    Add answer for Binary Tree Level order  Traversal II.

---
+++ Binary Search/Binary Tree Level Order Traversal II/Solution.cpp
@@ -0,0 +1,38 @@
+// LeetCode, Binary Tree Level Order Traversal II
+// Time complexity O(n)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int> > result;
+        if(root == nullptr) return result;
+
+        queue<TreeNode*> current, next;
+        vector<int> level; // elments in level level
+
+        current.push(root);
+        while (!current.empty()) {
+            while (!current.empty()) {
+                TreeNode* node = current.front();
+                current.pop();
+                level.push_back(node -> val);
+                if (node -> left != nullptr) next.push(node -> left);
+                if (node -> right != nullptr) next.push(node -> right);
+            }
+            result.push_back(level);
+            level.clear();
+            swap(next, current);
+        }
+        reverse(result.begin(), result.end());
+        
+        return result;
+    }
+};
---
+++ Binary Search/Binary Tree Level Order Traversal II/Solution.java
@@ -0,0 +1,38 @@
+// LeetCode, Binary Tree Level Order Traversal II
+// Time complexity O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ArrayList<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                level.add(head.val);
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+            }
+            result.add(level);
+        }
+        Collections.reverse(result);
+        
+        return result;
+    }
+}
