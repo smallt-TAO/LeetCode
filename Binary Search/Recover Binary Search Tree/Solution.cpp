bfe73166c16ce1228eacb310057247738bd03a46
smallt-TAO <llt5662720@163.com>
2016-09-01 22:37:23 +0800

    Answer
    Add answer for recover Binary Search Tree.

---
+++ Binary Search/Recover Binary Search Tree/Solution.cpp
@@ -0,0 +1,53 @@
+// LeetCode, Recover Binary Search Tree
+// Time complexity O(n)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        pair<TreeNode*, TreeNode*> broken;
+        TreeNode* prev = nullptr;
+        TreeNode* cur = root;
+        
+        while (cur != nullptr) {
+            if (cur -> left == nullptr) {
+                detect(broken, prev, cur);
+                prev = cur;
+                cur = cur -> right;
+            } else {
+                auto node = cur -> left;
+                while (node -> right != nullptr && node -> right != cur)
+                    node = node -> right;
+                if (node -> right == nullptr) {
+                    node -> right = cur;
+                    // prev = cur;
+                    cur = cur -> left;
+                } else {
+                    detect(broken, prev, cur);
+                    node -> right = nullptr;
+                    prev = cur;
+                    cur = cur -> right;
+                }
+             }
+        }
+        swap(broken.first -> val, broken.second -> val);
+    }
+    
+    void detect(pair<TreeNode*, TreeNode*>& broken, TreeNode* prev,
+            TreeNode* current) {
+        if (prev != nullptr && prev->val > current->val) {
+            if (broken.first == nullptr) {
+                broken.first = prev;
+            } //不能用else，例如 {0,1}，会导致最后 swap时second为nullptr，
+              //会 Runtime Error
+            broken.second = current;
+        }
+    }
+};
