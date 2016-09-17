0148392699c8db46db802e029f03ab7f7051cd63
smallt-TAO <llt5662720@163.com>
2016-09-17 16:47:52 +0800

    Answer
    Add answer for Sort List.

---
+++ Sort/Sort List/Solution.js
@@ -0,0 +1,80 @@
+﻿/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    if (head === null) {
+        return head;
+    }
+    return mergeSort(head);
+    
+    function mergeSort(items) {
+        if (items.next === null) {
+            return items;
+        }
+        var left = items;
+        var right = getMid(items);
+        return merge(mergeSort(left), mergeSort(right));
+    }
+    
+    function merge(left, right) {
+        var merge_result;
+        if (left === null) {
+            return right;
+        } else if (right === null) {
+            return left;
+        }
+        if (left.val < right.val) {
+            lead = left;
+            left = left.next;
+        } else {
+            lead = right;
+            right = right.next;
+        }
+        var tmp = lead;
+        while (left !== null && right !== null) {
+            if (left.val < right.val) {
+                tmp.next = left;
+                tmp = left;
+                left = left.next;
+            } else {
+                tmp.next = right;
+                tmp = right;
+                right = right.next;
+            }
+        }
+        if (left !== null) {
+            tmp.next = left;
+        } else {
+            tmp.next = right;
+        }
+        return lead;
+    }
+    
+    function getMid(first) {
+        var fast = first.next;
+        var slow = first.next;
+        var prev = first;
+        while (true) {
+            if (fast !== null)
+                fast = fast.next;
+            else
+                break;
+            if (fast !== null)
+                fast = fast.next;
+            else
+                break;
+            prev = slow;
+            slow = slow.next;
+        }
+        prev.next = null;  // cut
+        return slow;
+    }
+};
