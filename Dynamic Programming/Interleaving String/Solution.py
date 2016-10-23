8a454f5bd261d6ff3fa691a7460773cbdf1e403a
smallt-TAO <llt5662720@163.com>
2016-10-23 12:31:19 +0800

    Answer
    Add answer for Interleaving String.

---
+++ Dynamic Programming/Interleaving String/Solution.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        if s1 is None or s2 is None or s3 is None:
+            return False
+        if len(s1) + len(s2) != len(s3):
+            return False
+        interleave = [[False] * (len(s2) + 1) for i in range(len(s1) + 1)]
+        interleave[0][0] = True
+        for i in range(len(s1)):
+            interleave[i + 1][0] = s1[:i + 1] == s3[:i + 1]
+        for i in range(len(s2)):
+            interleave[0][i + 1] = s2[:i + 1] == s3[:i + 1]
+            
+        for i in range(len(s1)):
+            for j in range(len(s2)):
+                interleave[i + 1][j + 1] = False
+                if s1[i] == s3[i + j + 1]:
+                    interleave[i + 1][j + 1] = interleave[i][j + 1]
+                if s2[j] == s3[i + j + 1]:
+                    interleave[i + 1][j + 1] |= interleave[i + 1][j]
+        return interleave[len(s1)][len(s2)]
+        
