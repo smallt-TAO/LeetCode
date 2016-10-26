87e99f046f45fd9f994b1676da9747df375de1ec
smallt-TAO <llt5662720@163.com>
2016-10-26 21:07:04 +0800

    Answer
    Add answer for Decode Ways.

---
+++ Dynamic Programming/Decode Ways/Solution.js
@@ -0,0 +1,25 @@
+﻿/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    if (typeof s !== 'string' || s.length === 0) {
+        return 0;
+    }
+    
+    var len     = s.length;
+    var memo    = [];
+    memo[len]   = 1;
+    memo[len-1] = s[len-1] === '0' ? 0 : 1;
+
+    for (var i = len - 2 ; i >= 0 ; i--) {
+        if (s[i] === '0') {
+            memo[i] = 0;
+            continue;
+        }
+        var double = Number(s.substring(i, i + 2));
+        memo[i] = (double <= 26) ? memo[i + 1] + memo[i + 2] : memo[i + 1];
+    }
+
+    return memo[0];
+};
